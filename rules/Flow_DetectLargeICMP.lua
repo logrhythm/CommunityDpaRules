23fb20e879f4ffad2dd729a1d88b9dec358be680
Kjell Hedstrom <kjell.hedstrom@logrhythm.com>
2016-07-29 10:50:06 -0600

    updated ICMP rule with limit check for 160 bytes

--- rules/Flow_DetectLargeICMP.lua
+++ rules/Flow_DetectLargeICMP.lua
@@ -1,6 +1,6 @@
 function Flow_DetectLargeICMP (dpiMsg, ruleEngine)
-   -- Alarm if ICMP traffic contains abnormally large packets, indicative of 
-   -- ICMP tunneling traffic
+   -- Alarm if ICMP traffic contains abnormally large packets, 
+   -- indicative of ICMP tunneling traffic
     
 require 'LOG'
    local app = GetLatestApplication(dpiMsg)
@@ -10,7 +10,7 @@ require 'LOG'
       local numPackets = GetInt(dpiMsg, 'internal', 'totalpackets')
       if (numPackets) then
          local avgPktSize  = size / numPackets
-         if avgPktSize >= 1000 then
+         if avgPktSize >  160 then
             SetCustomField(dpiMsg, "ICMP_PacketSize", avgPktSize)
             WARNING(debug.getinfo(1, "S"), "Possible ICMP tunneling traffic detected. UUID: ",  GetUuid(dpiMsg),  ", Average packet size: ",  avgPktSize)
             TriggerUserAlarm(dpiMsg, ruleEngine, 'high')
